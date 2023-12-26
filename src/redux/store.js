185dd32d1bd8245dc10995c308c3e90511bff8f9
Dmytryi <muzalevskyid333@gmail.com>
2023-12-26 17:49:58 +0200

    ю

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import myContactsReducer from './myContacts/myContacts';
-
-export const store = configureStore({
-  reducer: {
-    myContacts: myContactsReducer,
-  },
-});
